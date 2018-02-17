b6ffe6b37d79b4198ad0b7085d1d645a7fa0057d
kono <konotatsuya@kono.local>
2018-02-17 15:43:54 +0900

    Put link of logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
-  resources :users, only: [:edit, :update] do
+  resources :users, only: [:edit, :update, :destroy] do
   resources :messages, only: [:index]
   end
 
