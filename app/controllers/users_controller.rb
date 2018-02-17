9bf9ce4c9cb1d96e0020d547f320e661bbaf5545
kono <konotatsuya@kono.local>
2018-02-17 12:49:33 +0900

    Correct edit function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,19 @@
 class UsersController < ApplicationController
+
   def edit
-    redirect_to messages: :index 
   end
 
   def update
-    @user = User.find(params[id])
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
