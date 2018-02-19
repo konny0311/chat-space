c9b79b7f19a604152b5c9db67dd151a96fe413dd
kono <konotatsuya@kono.local>
2018-02-19 18:58:27 +0900

    Message model created again

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,15 @@
 class MessagesController < ApplicationController
 
   def index
+    @message = current_user.messages
   end
 
+  def create
+    @message = Message.find(params[:id])
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:content, :image)
+  end
 end
