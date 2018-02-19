a052ab9c3f4fe69db8198df8d83589a82b75629b
kono <konotatsuya@kono.local>
2018-02-19 13:04:40 +0900

    Create group/group_user models and group controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :messages
+  has_many :group_users
+  has_many :groups, through: :group_users
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
