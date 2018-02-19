e28494b2df464925d2cebff298973a1ffbb0caa4
kono <konotatsuya@kono.local>
2018-02-19 16:06:23 +0900

    Change root setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'groups#index'
+  root 'messages#index'
   resources :users,     only: [:index, :edit, :update, :destroy]
   resources :groups,    only: [:new, :create, :edit, :update] do
   resources :messages,  only: [:index]
