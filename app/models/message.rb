3fc15e43ca4f1ff78fe350d5e0d586a85d04a140
kono <konotatsuya@kono.local>
2018-02-21 19:27:50 +0900

    Set factory girl for message model test

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,5 +4,7 @@ class Message < ApplicationRecord
   belongs_to :group, optional: true
   mount_uploader :image, ImageUploader
   validates :content, presence: true, unless: :image?
+  validates :group, presence: true
+  validates :user, presence: true
 
 end
---
+++ spec/factories/messages.rb
@@ -0,0 +1,9 @@
+FactoriGirl.define do
+
+  factory :message do
+    content   "hello"
+    image     Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/cat.jpg'))
+    group_id  "1"
+    user_id   "1"
+  end
+end
