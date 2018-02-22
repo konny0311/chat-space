e89e8c5411cff852f84180eb512a3cdc10f71a5c
kono <konotatsuya@kono.local>
2018-02-22 16:01:06 +0900

    Set sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :sessions => 'users/sessions'
+  }
   root 'groups#index'
   resources :users,     only: [:index, :edit, :update, :destroy]
   resources :groups,    only: [:index, :new, :create, :edit, :update] do
