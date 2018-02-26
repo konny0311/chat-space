c785102aac94ffd9c940da61d9ac3637626ab92e
kono <konotatsuya@kono.local>
2018-02-26 15:39:26 +0900

    Small progress of Ajax for message
    Ajax function done, but a posted message is not stored in a table and
    got wrong layout

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,20 @@
 $(function() {
+  function buildHTML(message){
+    var html = `
+    <li class="chat-each">
+      ${message.user_name}
+      <span class=posteddate>
+        ${message.created_at}
+        </span>
+      <p class=message>
+        ${message.content}
+        <%= image_tag message.image.url, class: 'image-message' if message.image.present? %>
+        </li>`
+        return html;
+  }
   $('#send_message').on('submit', function(e){
     e.preventDefault();
-    console.log(this);
+    console.log(this)
     var formData = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({
@@ -12,5 +25,10 @@ $(function() {
       processData: false,
       contentType: false
     })
-  });
-})
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.show_messages').append(html)
+      $('.form__message').val('')
+    })
+  })
+});
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -2,3 +2,4 @@ json.id @message.id
 json.content @message.content
 json.image @message.image
 json.created_at @message.created_at
+json.user_name @message.user.name
