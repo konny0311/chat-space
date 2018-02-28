6301b4c9ad08394c7916da46e9deedbf6357e885
kono <konotatsuya@kono.local>
2018-02-28 18:44:54 +0900

    Put Ajax fail case

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,4 +1,12 @@
 $(function() {
+  function searchHTML(user){
+    var html = `
+    <div class="chat-group-user clearfix">
+  <p class="chat-group-user__name">${user.name}</p>
+  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+</div>`
+return html;
+}
   $(".chat-group-form__input").on("keyup", function(){
     var input = $(".chat-group-form__input").val();
     console.log(input);
@@ -13,20 +21,20 @@ $(function() {
       $('#user-search-result').empty();
       if (users.length !== 0) {
         users.forEach(function(user) {
-          $('#user-search-result').append(
-            `<div class="username">
-             ${user.name}
-            </div>`
-          )
+          var html = searchHTML(user)
+          $('#user-search-result').append(html)
         })
       }
       else {
         $('#user-search-result').append(
-          `<div class="username">
-           一致するユーザーはいません
-          </div>`
+          `<div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">一致するユーザーはいません</p>
+      </div>`
         )
       }
-    });
+    })
+    .fail(function() {
+      alert('検索に失敗しました');
+    })
   })
 })
