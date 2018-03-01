37aaa914b1b4c4974741354cdaf564a4799c184b
kono <konotatsuya@kono.local>
2018-03-01 12:52:54 +0900

    "追加"btn working

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -24,6 +24,9 @@ return html;
           var html = searchHTML(user)
           $('#user-search-result').append(html)
         })
+        $('.chat-group-user').on('click',　'.chat-group-user__btn--add', function() {
+          console.log('test')
+        })
       }
       else {
         $('#user-search-result').append(
@@ -38,4 +41,15 @@ return html;
     })
     return false;
   })
+
+//     var user = $('.btn-add').val();
+//     $('#chat-group-seletcted-members').append(
+//       `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+// <input name='group[user_ids][]' type='hidden' value='${user.id}'>
+// <p class='chat-group-user__name'>${user.name}</p>
+// <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+// </div>`)
+
 })
+
+// メンバー追加処理は@group.users << user みたいな感じ
