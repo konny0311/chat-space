120db6fa2070693dada106775f7c5343803ffae0
kono <konotatsuya@kono.local>
2018-03-03 15:21:48 +0900

    Set insertHTML function for refactoring

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,30 +1,7 @@
 $(function() {
-  var interval = setInterval(function(){
-      if ($('.chat-history')) {
-      var id = $('.chat-history:last')[0].dataset.message_id
-    }
-      else {
-        var id = 0
-      }
-      $.ajax({
-        url: location.href,
-        type: 'GET',
-        data: {id: id},
-        dataType:'json'
-      })
-      .always(function(data) {
-        data.forEach(function(message) {
-          var html = buildHTML(message);
-          $('.contents_main--body').append(html)
-          if (message.image.url == null) $('.image-message').remove();
-          ('.contents_main--body').animate({scrollTop: $('.contents_main--body')[0].scrollHeight}, 300, 'swing')
-        })
-      })
-    }
-  , 5000)
   function buildHTML(message){
     var html_with_image = `
-    <ul class="chat-history">
+    <ul class="chat-history" data-message_id="${message.id}">
     <li class="chat-each">
       ${message.user_name}
       <span class=posteddate>
@@ -38,6 +15,34 @@ $(function() {
         </ul>`
     return html_with_image;
   }
+  function insertHTML(message){
+    var html = buildHTML(message);
+    $('.contents_main--body').append(html).trigger('create');
+    if (message.image.url == null) $('.image-message').remove();
+    $('.contents_main--body').animate({scrollTop: $('.contents_main--body')[0].scrollHeight}, 300, 'swing')
+    $('.form__message').val('')
+    $('#message_image').val('')
+  }
+  setInterval(function(){
+    if ($('.chat-history')) {
+      var id = $('.chat-history:last')[0].dataset.message_id
+    }
+    else {
+      var id = 0
+    }
+    $.ajax({
+      url: location.href,
+      type: 'GET',
+      data: {id: id},
+      dataType:'json'
+    })
+    .always(function(data) {
+      data.forEach(function(message) {
+        insertHTML(message);
+      })
+    })
+  }
+  , 5000)
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     console.log(this)
@@ -53,12 +58,7 @@ $(function() {
       cache: false
     })
     .done(function(data){
-      var html = buildHTML(data);
-      $('.contents_main--body').append(html).trigger('create');
-      if (data.image.url == null) $('.image-message').remove();
-      $('.form__message').val('')
-      $('#message_image').val('')
-      $('.contents_main--body').animate({scrollTop: $('.contents_main--body')[0].scrollHeight}, 300, 'swing')
+      insertHTML(data);
     })
     .fail(function() {
       alert('メッセージ送信に失敗しました');
